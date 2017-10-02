7abc637f38076a26b98f60701ca8a2e42c08930f
bernardohrl <bernardohrl.unb@gmail.com>
2017-10-02 01:32:42 -0300

    Removing Stopwords and Stemming from python

--- pre_processing/pre_processing.py
+++ pre_processing/pre_processing.py
@@ -2,10 +2,6 @@ import os
 import codecs
 from time import time
 import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
 path = '../dataset'
 data = [['corpus', 'label']]
@@ -25,8 +21,6 @@ for root, dirs, files in os.walk(path):
             content = re.sub('[^a-zA-Z]', ' ', content)
             content = content.lower()
             content = content.split()
-            ps = PorterStemmer()
-            content = [ps.stem(word) for word in content if not word in set(stopwords.words('english'))]
             content = ' '.join(content)
 
             row = []
